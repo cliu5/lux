282c84a64fd70addd85dca9e4f743556185b4647
cliu5 <cliu5@stuy.edu>
2019-06-14 23:38:46 -0400

    done

--- draw.py
+++ draw.py
@@ -2,7 +2,7 @@ from display import *
 from matrix import *
 from gmath import *
 import re
-
+#Pacy&Kevin from last year helped me w this homework
 def draw_scanline(x0, z0, x1, z1, y, screen, zbuffer, color):
     if x0 > x1:
         tx = x0
